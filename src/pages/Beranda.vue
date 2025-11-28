7097ad1a8f6800b7c6c3f3e96f92f6915bfa0802
taufikyogapratama <taufikpratama326@gmail.com>
2025-11-28 16:21:13 +0700

    add: beranda and calender pages

--- src/pages/Beranda.vue
+++ src/pages/Beranda.vue
@@ -1,11 +1,56 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Popover from "@/components/ui/popover/Popover.vue";
+import PopoverContent from "@/components/ui/popover/PopoverContent.vue";
+import PopoverTrigger from "@/components/ui/popover/PopoverTrigger.vue";
+import { ContactRound, EllipsisVertical, FolderClosed } from "lucide-vue-next";
+</script>
 <template>
-  <div class="flex flex-1 flex-col gap-4 p-4 pt-4">
-    <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-      <div class="aspect-video rounded-xl bg-gray-400" />
-      <div class="aspect-video rounded-xl bg-muted/50" />
-      <div class="aspect-video rounded-xl bg-muted/50" />
+  <div
+    class="flex justify-center md:justify-normal gap-5 md:mx-6 md:gap-10 flex-wrap mt-3"
+  >
+    <div class="w-80 rounded-xl border border-gray-300">
+      <header
+        class="bg-gray-500 rounded-t-xl h-24 flex flex-col justify-center px-4 relative"
+      >
+        <router-link
+          to="/pemrograman-web-praktik"
+          class="text-2xl text-white text-nowrap overflow-hidden hover:underline"
+          >Pemrograman Web Praktik (IX)</router-link
+        >
+        <p class="text-white text-nowrap overflow-hidden hover:underline">
+          Jhon Doe
+        </p>
+        <!-- <img src="" alt=""> -->
+        <div
+          class="w-18 h-18 bg-green-500 rounded-full absolute right-5 -bottom-9"
+        ></div>
+      </header>
+      <main class="h-20"></main>
+      <footer
+        class="flex justify-end gap-2 px-3 h-16 items-center rounded-b-xl border-t border-gray-300"
+      >
+        <button class="p-2 rounded-full hover:bg-gray-200">
+          <contact-round />
+        </button>
+        <button class="p-2 rounded-full hover:bg-gray-200">
+          <folder-closed />
+        </button>
+        <Popover>
+          <PopoverTrigger as-child>
+            <button class="p-2 rounded-full hover:bg-gray-200">
+              <ellipsis-vertical />
+            </button>
+          </PopoverTrigger>
+          <PopoverContent class="w-auto px-0 py-2">
+            <router-link class="block hover:bg-gray-200 p-2" to="#"
+              >Batalkan pendaftaran</router-link
+            >
+            <router-link class="block hover:bg-gray-200 p-2" to="#"
+              >Laporkan penyalahgunaan</router-link
+            >
+          </PopoverContent>
+        </Popover>
+      </footer>
     </div>
-    <div class="min-h-screen flex-1 rounded-xl bg-muted/50 md:min-h-min" />
   </div>
 </template>
--- src/pages/Kalender.vue
+++ src/pages/Kalender.vue
@@ -1,4 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { DateValue } from "@internationalized/date";
+import { getLocalTimeZone, today } from "@internationalized/date";
+import { Calendar } from "@/components/ui/calendar";
+import { ref, type Ref } from "vue";
+
+const date = ref(today(getLocalTimeZone())) as Ref<DateValue>;
+const defaultPlaceholder = today(getLocalTimeZone());
+</script>
+
 <template>
-  <div>Ini Kalender</div>
+  <div>
+    <div class="flex justify-center md:justify-normal md:px-5 mt-5 mb-5">
+      <select class="min-w-80 h-10 border">
+        <option value="Semua kelas">Semua kelas</option>
+        <option value="pemrograman web praktik">
+          Pemrograman Web Praktik (IX)
+        </option>
+      </select>
+    </div>
+    <div>
+      <Calendar
+        v-model="date"
+        :default-placeholder="defaultPlaceholder"
+        weekday-format="short"
+        class="shadow-sm **:data-[slot=calendar-cell-trigger]:size-12!"
+      ></Calendar>
+    </div>
+  </div>
 </template>
